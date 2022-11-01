beee22967ee0d488d19d2a774bfa3685de980b1a
Md. Riajul Islam <riaj17021995@gmail.com>
2022-11-01 22:29:54 +0600

    update

--- server.js
+++ server.js
@@ -6,32 +6,26 @@ const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const userProfileRoutes = require("./routes/userProfileRoutes");
 const courseRoutes = require("./routes/coursesRoutes");
-const enrollRoutes = require("./routes/enrollRoutes")
+const enrollRoutes = require("./routes/enrollRoutes");
 
 // express app
 const app = express();
 
-
 // middleware
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
-
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 // route
 app.get("/", (req, res) => {
   res.status(200).json({ api: "Life Spring API 🌳" });
 });
 
-
 // routes for user
 app.use("/api/auth", userRoutes);
 app.use("/api/user/profile", userProfileRoutes);
 app.use("/api/courses", courseRoutes);
-app.use("/api/enroll", enrollRoutes)
-
-
+app.use("/api/enroll", enrollRoutes);
 
 // connect to DB
 mongoose
